aae6bd54bcdacf3d44031a160fcbcc28c1bf7f0c
Florenica Marbian <maria.marbian@davinci.edu.ar>
2023-02-15 16:49:40 -0300

    Responsive index/iniciar-sesion index/detalle

--- guggiari-micaela_marbian-mariaflorencia_final/sitio/vistas/iniciar-sesion.php
+++ guggiari-micaela_marbian-mariaflorencia_final/sitio/vistas/iniciar-sesion.php
@@ -23,6 +23,10 @@ unset($_SESSION['data_form']);
                 <button type="submit" class="button btn btn-primary">Ingresar</button>
             </div>
         </form>
-        <p>¿Olvidaste tu password? <a href="index.php?v=recuperar-password">Solicitá restablecer tu password</a> </p>
+        <div class="pass">
+            <p>¿Olvidaste tu password?</p>
+            <a href="index.php?v=recuperar-password">Solicitá restablecer tu password</a>
+        </div>
+
     </div>
 </div>
