1472e8b2306af98531b561f46cbc4d850e1692e4
Micaela Guggiari <102701164+mguggiari@users.noreply.github.com>
2023-03-03 18:28:25 -0300

    correcciones html

--- guggiari-micaela_marbian-mariaflorencia_final/sitio/index.php
+++ guggiari-micaela_marbian-mariaflorencia_final/sitio/index.php
@@ -70,33 +70,12 @@ unset($_SESSION['mensaje_exito'], $_SESSION['mensaje_error']);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/estilos.css">
-<<<<<<< Updated upstream
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet" />
-    <link rel="shortcut icon" href="imgs/logo.png" type="image/x-icon" />
-
-    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            setTimeout(function() {
-                $(".msg-success").fadeOut(1500);
-            },3000);
-
-
-        });
-    </script>
-
-=======
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="imgs/logo.png" type="image/x-icon">
->>>>>>> Stashed changes
 </head>
-
 <body>
     <header class="navbar navbar-expand-md bd-navbar">
         <nav class="container-xxl flex-wrap flex-md-nowrap" aria-label="navegacion principal">
@@ -225,6 +204,14 @@ unset($_SESSION['mensaje_exito'], $_SESSION['mensaje_error']);
             </div>
         </div>
     </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                $(".msg-success").fadeOut(1500);
+            },3000);
+        });
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- guggiari-micaela_marbian-mariaflorencia_final/sitio/vistas/contacto.php
+++ guggiari-micaela_marbian-mariaflorencia_final/sitio/vistas/contacto.php
@@ -6,15 +6,15 @@
     <form action="recursos/enviar.php" method="get" class="row g-3 text-center text-md-start container-form">
         <div class="col-xl-7">
             <label class="form-label" for="nombre">Nombre</label>
-            <input class="form-control" name="nombre" id="nombre" type="text" required />
+            <input class="form-control" name="nombre" id="nombre" type="text" required>
         </div>
         <div class="col-xl-7">
             <label class="form-label" for="apellido">Apellido</label>
-            <input class="form-control" name="apellido" id="apellido" type="text" required />
+            <input class="form-control" name="apellido" id="apellido" type="text" required>
         </div>
         <div class="col-xl-7">
             <label class="form-label" for="clave">Mail</label>
-            <input class="form-control" name="clave" id="clave" type="email" required />
+            <input class="form-control" name="clave" id="clave" type="email" required>
         </div>
 
         <div class="col-xl-8">
@@ -24,7 +24,7 @@
 
         <div class="col-xl-7">
             <label class="form-check-label" for="sobre">Desea recibir información</label>
-            <input class="form-check-label" type="checkbox" name="sobre" id="sobre" />
+            <input class="form-check-label" type="checkbox" name="sobre" id="sobre">
         </div>
 
         <div>
