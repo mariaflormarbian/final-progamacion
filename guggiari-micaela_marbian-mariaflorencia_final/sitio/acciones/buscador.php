a8c619b716d13e2dcc904f48aaf9d51931d3a522
Florenica Marbian <maria.marbian@davinci.edu.ar>
2023-03-16 00:09:03 -0300

    Buscador

--- guggiari-micaela_marbian-mariaflorencia_final/sitio/acciones/buscador.php
+++ guggiari-micaela_marbian-mariaflorencia_final/sitio/acciones/buscador.php
@@ -1,8 +1,12 @@
 <?php
+
 // Conexión a la base de datos
 $conexion = mysqli_connect("127.0.0.1", "root", "", "dw3_guggiari_marbian");
 
 // Recuperar el término de búsqueda
+
+
+
 $termino_busqueda = $_GET['termino_busqueda'];
 
 // Realizar la consulta
@@ -11,7 +15,9 @@ $resultado = mysqli_query($conexion, $query);
 
 // Mostrar los resultados en una tabla
 echo "<table>";
-while ($fila = mysqli_fetch_array($resultado)) {
+
+foreach ($resultado as $fila) {
+
     echo "<tr><td>" . $fila['titulo'] . "</td><td>" . $fila['imagen'] . "</td></tr>";
 
 }
@@ -19,4 +25,10 @@ echo "</table>";
 
 // Cerrar la conexión a la base de datos
 mysqli_close($conexion);
+
+
+
 ?>
+
+
+
